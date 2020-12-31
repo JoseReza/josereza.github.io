604d217cfe31ab8b480c158af53193bfb517cfc0
JoseReza <65379587+JoseReza@users.noreply.github.com>
2020-12-30 19:44:28 -0800

    Delete index.js

--- proyects/index.js
+++
@@ -1,5 +0,0 @@
-var btn_vision_artificial_knn = document.getElementById("btn-vision-artificial-knn");
-
-btn_vision_artificial_knn.addEventListener('click', function(){
-    location.replace("./artificial_vision_knn/index.html")
-})
