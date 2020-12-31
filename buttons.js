8e9738a9da46171e03b903365ab2e399bc29cf60
JoseReza <65379587+JoseReza@users.noreply.github.com>
2020-12-30 19:53:10 -0800

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,46 @@
+var dominio = 'https://josereza.github.io';
+
+var trayectoria = document.getElementById("trayectoria");
+
+var btn_inicio = document.getElementById("btn-inicio");
+var btn_electronica = document.getElementById("btn-electronica");
+var btn_mecanica = document.getElementById("btn-mecanica");
+var btn_programacion = document.getElementById("btn-programacion");
+var btn_control = document.getElementById("btn-control");
+var btn_comunicaciones = document.getElementById("btn-comunicaciones");
+var btn_certificaciones = document.getElementById("btn-certificaciones");
+var btn_proyectos = document.getElementById("btn-proyectos");
+
+var inicio = page.innerHTML;
+
+btn_inicio.addEventListener("click", () => {
+    page.innerHTML = inicio;
+});
+
+btn_electronica.addEventListener("click", () => {
+  location.replace(dominio + "/electronics/index.html")
+});
+
+btn_mecanica.addEventListener("click", () => {
+  location.replace(dominio + "/mecanics/index.html")
+});
+
+btn_programacion.addEventListener("click", () => {
+  location.replace(dominio + "/programming/index.html")
+});
+btn_control.addEventListener("click", () => {
+
+  location.replace(dominio + "/control.html")
+});
+
+btn_comunicaciones.addEventListener("click", () => {
+  location.replace(dominio + "/telecommunications/index.html")
+});
+
+btn_certificaciones.addEventListener("click", () => {
+  location.replace(dominio + "/certifications/index.html")
+});
+
+btn_proyectos.addEventListener("click", () => {
+  location.replace(dominio + "/proyects/index.html")
+});
---
+++ navbar.js
@@ -0,0 +1,45 @@
+var navbar = document.getElementById("navbar");
+
+navbar.innerHTML = `
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <img src="src/icon.png" width="50" height="50">
+        &nbsp;&nbsp;&nbsp;
+        <a class="navbar-brand" href="#">Hoja de experiencia</a>
+        <button onclick="function1()" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse text-center" id="navbarColor01">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" id= "btn-inicio">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id= "btn-electronica">Electrónica</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id= "btn-mecanica">Mecánica</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id= "btn-programacion">Programación</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id= "btn-control">Control</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id= "btn-comunicaciones">Comunicaciones</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id= "btn-certificaciones">Certificaciones</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id= "btn-proyectos">Proyectos</a>
+            </li>
+          </ul>
+        </div>
+              <script type="text/javascript">
+                function	function1(){ document.getElementById("navbarColor01").classList.toggle('show');}
+              </script>
+      </nav>
+
+`;
