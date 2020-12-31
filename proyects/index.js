fcacb61582bc6e52c249ecb923f0604e8f82ebde
JoseReza <65379587+JoseReza@users.noreply.github.com>
2020-12-30 19:52:42 -0800

    Add files via upload

---
+++ proyects/index.js
@@ -0,0 +1,5 @@
+var btn_vision_artificial_knn = document.getElementById("btn-vision-artificial-knn");
+
+btn_vision_artificial_knn.addEventListener('click', function(){
+    location.replace("./artificial_vision_knn/index.html")
+})
