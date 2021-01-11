62be695f6fd8a36f8cb59b1cd2bd034aeeff15f5
JoseReza <65379587+JoseReza@users.noreply.github.com>
2021-01-10 23:16:45 -0700

    Update buttons.js

--- proyects/buttons.js
+++ proyects/buttons.js
@@ -1,5 +1,6 @@
 var btn_vision_artificial_knn = document.getElementById("btn-vision-artificial-knn");
 var btn_reconocimiento_facial = document.getElementById("btn-reconocimiento-facial");
+var btn_plc_node = document.getElementById("btn-plc-node");
 
 btn_vision_artificial_knn.addEventListener('click', function(){
     location.replace("./artificial_vision_knn/index.html")
@@ -7,4 +8,8 @@ btn_vision_artificial_knn.addEventListener('click', function(){
 
 btn_reconocimiento_facial.addEventListener('click', function(){
     location.replace("./face_recognition/index.html")
+})
+
+btn_plc_node.addEventListener('click', function(){
+    location.replace("./plc_node/index.html")
 })
