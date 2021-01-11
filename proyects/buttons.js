b4a8d8315acec4a61e88360d9220bf3048c2cdef
JoseReza <65379587+JoseReza@users.noreply.github.com>
2021-01-11 00:06:11 -0700

    Update buttons.js

--- proyects/buttons.js
+++ proyects/buttons.js
@@ -1,6 +1,7 @@
 var btn_vision_artificial_knn = document.getElementById("btn-vision-artificial-knn");
 var btn_reconocimiento_facial = document.getElementById("btn-reconocimiento-facial");
 var btn_plc_node = document.getElementById("btn-plc-node");
+var btn_investigacion_iiot = document.getElementById("btn-investigacion-iiot");
 
 btn_vision_artificial_knn.addEventListener('click', function(){
     location.replace("./artificial_vision_knn/index.html")
@@ -12,4 +13,8 @@ btn_reconocimiento_facial.addEventListener('click', function(){
 
 btn_plc_node.addEventListener('click', function(){
     location.replace("./plc_node/index.html")
+})
+
+btn_investigacion_iiot.addEventListener('click', function(){
+    location.replace("./investigacion_iiot/index.html")
 })
