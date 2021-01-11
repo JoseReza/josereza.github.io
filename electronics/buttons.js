ebea39c37708e394613db859f674d0abf8d45c32
JoseReza <65379587+JoseReza@users.noreply.github.com>
2021-01-11 14:12:12 -0700

    Add files via upload

---
+++ electronics/buttons.js
@@ -0,0 +1,5 @@
+var btn_ver_diseno_electronico = document.getElementById("btn-ver-diseno-electronico");
+
+btn_ver_diseno_electronico.addEventListener("click", ()=>{
+    location.replace("http://josereza.github.io/electronics/designs.html");
+})
