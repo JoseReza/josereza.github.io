176078dc8cc5124586a9a0b038ead4cb6489b423
JoseReza <65379587+JoseReza@users.noreply.github.com>
2021-01-11 15:37:10 -0700

    Add files via upload

---
+++ mecanics/buttons.js
@@ -0,0 +1,5 @@
+var btn_ver_diseno_mecanico = document.getElementById("btn-ver-diseno-mecanico");
+
+btn_ver_diseno_mecanico.addEventListener("click", ()=>{
+    location.replace("http://josereza.github.io/mecanics/designs.html");
+})
